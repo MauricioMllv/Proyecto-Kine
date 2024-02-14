129475e08074161b54715ee7b88a744a2410f9b6
Mauricio <m.millavil.escobar@gmail.com>
2024-02-14 02:36:16 -0300

    tailwind

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,19 @@
+import './style.css';
+
 const Navbar = () => {
     return (
         <div>
-            <p>KineHome</p>
+            <nav className="bg-blue-900 p-4">
+                <div className='container mx-auto flex flex-row justify-between items-center ps-10'>
+                <img src="" alt="" />
+                </div>
+                <div className='flex items-center space-x-4 justify-end text-white pe-10'>
+                <a href="" >Inicio</a>
+                <a href="" >Contacto</a>
+                <a href="" >Nosotros</a>
+                <a href="" >FAQ</a>
+                </div>
+            </nav>
         </div>
     );
 };
