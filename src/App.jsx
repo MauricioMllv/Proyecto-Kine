0f3b84e3b57647859da70906f645846ef7d85cde
Mauricio <m.millavil.escobar@gmail.com>
2024-02-20 14:04:15 -0300

    img

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar'
+import Carousel from './components/Header'
 import './App.css'
 
 function App() {
   return (
     <>
      <Navbar />
+     <Carousel />
     </>
   )
 }
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,20 @@
+import Banner from '../../assets/img/Banner.png';
+
+
+function Carousel () {
+    const slides = [
+        {
+            {Banner}
+        },
+        {
+            {Banner}
+        },
+        {
+            {Banner}
+        },
+    ];
+    return 
+    <div className='max-w-'>
+
+    </div>
+}
