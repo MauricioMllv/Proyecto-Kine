d0622d8d643cfc5d6abd9ff019f76dedc85bc530
Mauricio <m.millavil.escobar@gmail.com>
2024-02-23 23:36:14 -0300

    responsivo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import images1 from '../../assets/img/Banner.png';
 import images2 from '../../assets/img/enlaces.png';
-import right from '../../assets/img/right.png'
-import left from '../../assets/img/left.png'
+import right from '../../assets/img/right.png';
+import left from '../../assets/img/left.png';
 
 const Carousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const images = [
-    images1,
-    images2,
-    images1,
-  ];
+  const images = [images1, images2, images1];
 
   const nextSlide = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
@@ -32,12 +28,12 @@ const Carousel = () => {
   }, [currentIndex]);
 
   return (
-    <div className="flex items-center justify-center h-full p-10 ">
-      <button className='relative left-10 transform -translate-y-1/2 rounded-full transition duration-300 ease-in-out hover:bg-gray-300 active:bg-gray-500' onClick={prevSlide}><img className='size-8 ' src={left} alt="" /></button>
-      <div className="slide w-3/4 h-[500px] bg-center bg-cover overflow-hidden shadow-2xl" style={{ backgroundImage: `url(${images[currentIndex]})` }}>
-        <p>Lorem ipsum dolor sit amet consectetur.</p>
+    <div className="flex items-center justify-center h-full p-5 md:p-10">
+      <button className='hidden md:block relative left-5 md:left-10 transform -translate-y-1/2 rounded-full transition duration-300 ease-in-out hover:bg-gray-300 active:bg-gray-500' onClick={prevSlide}><img className='size-6 md:size-8' src={left} alt="" /></button>
+      <div className="slide w-full md:w-3/4 h-[150px] md:h-[500px] bg-center bg-cover overflow-hidden shadow-2xl" style={{ backgroundImage: `url(${images[currentIndex]})` }}>
+      {/* revisay alturas y anchos para diferentes pantallas  */}
       </div>
-      <button className='relative right-10 transform -translate-y-1/2 rounded-full transition duration-300 ease-in-out hover:bg-gray-300 active:bg-gray-500' onClick={nextSlide}><img className='size-8' src={right} alt="" /></button>
+      <button className='hidden md:block relative right-5 md:right-10 transform -translate-y-1/2 rounded-full transition duration-300 ease-in-out hover:bg-gray-300 active:bg-gray-500' onClick={nextSlide}><img className='size-6 md:size-8' src={right} alt="" /></button>
     </div>
   );
 };
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,17 +1,37 @@
-
+import { useState } from 'react';
 
 const Navbar = () => {
+    const [isMenuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!isMenuOpen);
+    };
+
     return (
         <div>
             <nav className="bg-[#084D6E] p-4">
-                <div className='container mx-auto flex flex-row justify-between items-center ps-10'>
-                <img src="" alt="" />
+                <div className="container mx-auto flex flex-row justify-between items-center ps-10">
+                    <img src="" alt="" />
+                    <div className="lg:hidden">
+                        <button
+                            className="text-white focus:outline-none"
+                            onClick={toggleMenu}
+                        >
+                            ☰
+                        </button>
+                    </div>
                 </div>
-                <div className='flex items-center space-x-4 justify-end text-white pe-10'>
-                <a href="" >Inicio</a>
-                <a href="" >Contacto</a>
-                <a href="" >Nosotros</a>
-                <a href="" >FAQ</a>
+                <div
+                    className={`${
+                        isMenuOpen
+                            ? 'block flex flex-col items-center space-y-4'
+                            : 'hidden'
+                    } lg:flex lg:items-center lg:space-x-4 justify-end text-white pe-10`}
+                >
+                    <a href="">Inicio</a>
+                    <a href="">Contacto</a>
+                    <a href="">Nosotros</a>
+                    <a href="">FAQ</a>
                 </div>
             </nav>
         </div>
